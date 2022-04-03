5c5ba36e1d7575cf916881ced89997bf1b77fb8e
marimnzs <marimnzsdeveloper@gmail.com>
2022-04-03 19:52:53 -0300

    adicionado intergracao com api

--- lib/app/shared/auth_store.dart
+++ lib/app/shared/auth_store.dart
@@ -1,12 +1,14 @@
 import 'package:mobx/mobx.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 part 'auth_store.g.dart';
 
 class AuthStore = _AuthStoreBase with _$AuthStore;
 
 abstract class _AuthStoreBase with Store {
-
   @action
   Future<bool> isLogged() async {
-    return false;
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    var isLogged = sharedPreferences.getString('token') != null;
+    return isLogged;
   }
 }
